c7a29d3bef5f9cd06e137acd2dc88f4204d69468
Taro Oshiro <toshiro470@mycr.redwoods.edu>
2022-09-03 12:40:54 -0700

    Completed a method used to estimate pi based on the number of sides of a polygon.

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+        double sides = 8;
+        double angleB = 360/sides;
+        double angleA = angleB/2;
+        double triangleBase = 2 * Math.sin(Math.toRadians(angleA));
+        double perimeter = sides * triangleBase;
+        double pi = perimeter/2;
+        System.out.println(pi);
 
     }
 }
